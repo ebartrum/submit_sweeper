ca70606c83b82c2f8c9f4df93ebac87b19aad152
Ed Bartrum <edward.bartrum@gmail.com>
2021-01-08 14:26:04 +0000

    Extract processing of sweep commands to funciton

--- sweep.py
+++ sweep.py
@@ -1,23 +1,26 @@
 import itertools
 
+def get_command_list(input):
+    words = input.split(" ")
+    shared_command = ' '.join(filter(lambda w: '=' not in w, words))
+    sweep_dict = {}
+    for word in words:
+        if '=' in word:
+            assert len(word.split('=')) == 2
+            arg_name = word.split('=')[0]
+            value_list = word.split('=')[1].split(',')
+            sweep_dict[arg_name] = value_list
+    arg_assignment_sweep_list = []
+    for k,v in sweep_dict.items():
+        arg_assignment_sweep_list.append([f"{k}={val}" for val in v])
+
+    command_list = []
+    for element in itertools.product(*arg_assignment_sweep_list):
+        command_list.append(shared_command + " " + " ".join(element))
+    return command_list
+
 input = "foo bar a=2,3 x=8,9"
-words = input.split(" ")
-shared_command = ' '.join(filter(lambda w: '=' not in w, words))
-sweep_dict = {}
-for word in words:
-    if '=' in word:
-        assert len(word.split('=')) == 2
-        arg_name = word.split('=')[0]
-        value_list = word.split('=')[1].split(',')
-        sweep_dict[arg_name] = value_list
-arg_assignment_sweep_list = []
-for k,v in sweep_dict.items():
-    arg_assignment_sweep_list.append([f"{k}={val}" for val in v])
-
-command_list = []
-for element in itertools.product(*arg_assignment_sweep_list):
-    command_list.append(shared_command + " " + " ".join(element))
-
+command_list = get_command_list(input)
 print(f"Submitting job array with following {len(command_list)} expts")
 for com in command_list:
     print(com)
